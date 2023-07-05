7b95c564e54da7c5f9a666b305e0dfff0d60e920
lucaslamas-13 <lucaslamas13@gmail.com>
2023-07-05 14:40:25 -0300

    feat: addresses created

--- addresses/models.py
+++ addresses/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 
 class Address(models.Model):
-    ...
+    street = models.CharField(max_length=150)
+    number = models.PositiveSmallIntegerField()
+    cep = models.CharField(max_length=8)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=2)
+    complement = models.CharField(max_length=150, null=True)
--- addresses/views.py
+++ addresses/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import AddressSerializer
+from .models import Address
 
 # Create your views here.
+
+
+class AdressView(generics.CreateAPIView):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer

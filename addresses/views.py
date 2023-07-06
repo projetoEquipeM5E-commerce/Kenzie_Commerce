93b8c9cbae92960aba9c6ce823e489655b41f902
lucaslamas-13 <lucaslamas13@gmail.com>
2023-07-06 16:58:44 -0300

    feat: relation Address with User

--- addresses/views.py
+++ addresses/views.py
@@ -5,11 +5,3 @@ from .models import Address
 from drf_spectacular.utils import extend_schema
 # Create your views here.
 
-
-class AdressView(generics.CreateAPIView):
-    queryset = Address.objects.all()
-    serializer_class = AddressSerializer
-
-    @extend_schema(operation_id="addresses_post", description="Rota de criar addresses no album", summary="Criar address")
-        def post(self, request, *args, **kwargs):
-            return self.create(request, *args, **kwargs)
